875f1e14f11c9a6d6120658f385d3f41d4825111
christophergoot <christophergoot@gmail.com>
2017-11-06 10:43:19 -0800

    location form

--- picnic.js
+++ picnic.js
@@ -1,5 +1,456 @@
-function picnicInThePark() {
+const YELP_PARK_RESULTS ={
+    "businesses": [
+        {
+            "id": "clinton-park-portland",
+            "name": "Clinton Park",
+            "image_url": "https://s3-media4.fl.yelpcdn.com/bphoto/Uyqtoi5d5ouxcOPHmf6otw/o.jpg",
+            "is_closed": false,
+            "url": "https://www.yelp.com/biz/clinton-park-portland?adjust_creative=Q9m-GQZI6WeFiuWS3dtooQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Q9m-GQZI6WeFiuWS3dtooQ",
+            "review_count": 4,
+            "categories": [
+                {
+                    "alias": "parks",
+                    "title": "Parks"
+                }
+            ],
+            "rating": 4.5,
+            "coordinates": {
+                "latitude": 45.5014,
+                "longitude": -122.60602
+            },
+            "transactions": [],
+            "location": {
+                "address1": "SE 55th Ave & Woodward St",
+                "address2": "",
+                "address3": "",
+                "city": "Portland",
+                "zip_code": "97206",
+                "country": "US",
+                "state": "OR",
+                "display_address": [
+                    "SE 55th Ave & Woodward St",
+                    "Portland, OR 97206"
+                ]
+            },
+            "phone": "+15038232525",
+            "display_phone": "(503) 823-2525",
+            "distance": 379.38726000599996
+        },
+        {
+            "id": "essex-park-portland",
+            "name": "Essex Park",
+            "image_url": "https://s3-media3.fl.yelpcdn.com/bphoto/HakOog0pubb7OiRbal2X7g/o.jpg",
+            "is_closed": false,
+            "url": "https://www.yelp.com/biz/essex-park-portland?adjust_creative=Q9m-GQZI6WeFiuWS3dtooQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Q9m-GQZI6WeFiuWS3dtooQ",
+            "review_count": 2,
+            "categories": [
+                {
+                    "alias": "parks",
+                    "title": "Parks"
+                },
+                {
+                    "alias": "playgrounds",
+                    "title": "Playgrounds"
+                }
+            ],
+            "rating": 4,
+            "coordinates": {
+                "latitude": 45.5000495910645,
+                "longitude": -122.581916809082
+            },
+            "transactions": [],
+            "location": {
+                "address1": "SE 79th Ave Ctr St",
+                "address2": "",
+                "address3": "",
+                "city": "Portland",
+                "zip_code": "97206",
+                "country": "US",
+                "state": "OR",
+                "display_address": [
+                    "SE 79th Ave Ctr St",
+                    "Portland, OR 97206"
+                ]
+            },
+            "phone": "+15038232525",
+            "display_phone": "(503) 823-2525",
+            "distance": 379.38726000599996
+        },
+        {
+            "id": "mt-scott-park-portland",
+            "name": "Mt Scott Park",
+            "image_url": "https://s3-media4.fl.yelpcdn.com/bphoto/wITfCx0BBIZiEm-ySctsJw/o.jpg",
+            "is_closed": false,
+            "url": "https://www.yelp.com/biz/mt-scott-park-portland?adjust_creative=Q9m-GQZI6WeFiuWS3dtooQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Q9m-GQZI6WeFiuWS3dtooQ",
+            "review_count": 7,
+            "categories": [
+                {
+                    "alias": "parks",
+                    "title": "Parks"
+                }
+            ],
+            "rating": 3.5,
+            "coordinates": {
+                "latitude": 45.4831854019548,
+                "longitude": -122.6047076
+            },
+            "transactions": [],
+            "location": {
+                "address1": "SE 72nd Ave Harold St",
+                "address2": "",
+                "address3": "",
+                "city": "Portland",
+                "zip_code": "97206",
+                "country": "US",
+                "state": "OR",
+                "display_address": [
+                    "SE 72nd Ave Harold St",
+                    "Portland, OR 97206"
+                ]
+            },
+            "phone": "+15038232525",
+            "display_phone": "(503) 823-2525",
+            "distance": 452.5431410398
+        },
+        {
+            "id": "laurelwood-park-portland",
+            "name": "Laurelwood Park",
+            "image_url": "https://s3-media2.fl.yelpcdn.com/bphoto/EyTJ_5unyPAUxWixLdYYSg/o.jpg",
+            "is_closed": false,
+            "url": "https://www.yelp.com/biz/laurelwood-park-portland?adjust_creative=Q9m-GQZI6WeFiuWS3dtooQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Q9m-GQZI6WeFiuWS3dtooQ",
+            "review_count": 3,
+            "categories": [
+                {
+                    "alias": "parks",
+                    "title": "Parks"
+                }
+            ],
+            "rating": 3,
+            "coordinates": {
+                "latitude": 45.4896,
+                "longitude": -122.59666
+            },
+            "transactions": [],
+            "location": {
+                "address1": "SE 64th St & Foster Rd",
+                "address2": "",
+                "address3": "",
+                "city": "Portland",
+                "zip_code": "97206",
+                "country": "US",
+                "state": "OR",
+                "display_address": [
+                    "SE 64th St & Foster Rd",
+                    "Portland, OR 97206"
+                ]
+            },
+            "phone": "",
+            "display_phone": "",
+            "distance": 613.0568779921999
+        },
+        {
+            "id": "brentwood-park-portland",
+            "name": "Brentwood Park",
+            "image_url": "https://s3-media1.fl.yelpcdn.com/bphoto/_cjySo1ENpkttBw50r5fTQ/o.jpg",
+            "is_closed": false,
+            "url": "https://www.yelp.com/biz/brentwood-park-portland?adjust_creative=Q9m-GQZI6WeFiuWS3dtooQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Q9m-GQZI6WeFiuWS3dtooQ",
+            "review_count": 16,
+            "categories": [
+                {
+                    "alias": "dog_parks",
+                    "title": "Dog Parks"
+                }
+            ],
+            "rating": 4,
+            "coordinates": {
+                "latitude": 45.4758106949955,
+                "longitude": -122.602229327093
+            },
+            "transactions": [],
+            "location": {
+                "address1": "SE 60th Ave And Duke St",
+                "address2": "",
+                "address3": "",
+                "city": "Portland",
+                "zip_code": "97206",
+                "country": "US",
+                "state": "OR",
+                "display_address": [
+                    "SE 60th Ave And Duke St",
+                    "Portland, OR 97206"
+                ]
+            },
+            "phone": "",
+            "display_phone": "",
+            "distance": 986.484864632
+        },
+        {
+            "id": "woodstock-park-portland-2",
+            "name": "Woodstock Park",
+            "image_url": "https://s3-media2.fl.yelpcdn.com/bphoto/s2ax2wTNoa5v4Q7jXvEs4w/o.jpg",
+            "is_closed": false,
+            "url": "https://www.yelp.com/biz/woodstock-park-portland-2?adjust_creative=Q9m-GQZI6WeFiuWS3dtooQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Q9m-GQZI6WeFiuWS3dtooQ",
+            "review_count": 1,
+            "categories": [
+                {
+                    "alias": "parks",
+                    "title": "Parks"
+                }
+            ],
+            "rating": 4,
+            "coordinates": {
+                "latitude": 45.4835943206779,
+                "longitude": -122.612352227261
+            },
+            "transactions": [],
+            "location": {
+                "address1": "SE 47th Ave Steels St",
+                "address2": "",
+                "address3": "",
+                "city": "Portland",
+                "zip_code": "97204",
+                "country": "US",
+                "state": "OR",
+                "display_address": [
+                    "SE 47th Ave Steels St",
+                    "Portland, OR 97204"
+                ]
+            },
+            "phone": "",
+            "display_phone": "",
+            "distance": 1030.869093893
+        },
+        {
+            "id": "kern-park-portland",
+            "name": "Kern Park",
+            "image_url": "https://s3-media1.fl.yelpcdn.com/bphoto/EG-A9n50a3KDvV7PhHMoKQ/o.jpg",
+            "is_closed": false,
+            "url": "https://www.yelp.com/biz/kern-park-portland?adjust_creative=Q9m-GQZI6WeFiuWS3dtooQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Q9m-GQZI6WeFiuWS3dtooQ",
+            "review_count": 1,
+            "categories": [
+                {
+                    "alias": "parks",
+                    "title": "Parks"
+                }
+            ],
+            "rating": 3,
+            "coordinates": {
+                "latitude": 45.4814224243164,
+                "longitude": -122.598579406738
+            },
+            "transactions": [],
+            "location": {
+                "address1": "67TH SE Rural St",
+                "address2": "",
+                "address3": "",
+                "city": "Portland",
+                "zip_code": "97206",
+                "country": "US",
+                "state": "OR",
+                "display_address": [
+                    "67TH SE Rural St",
+                    "Portland, OR 97206"
+                ]
+            },
+            "phone": "",
+            "display_phone": "",
+            "distance": 1057.240946138
+        },
+        {
+            "id": "woodstock-park-portland",
+            "name": "Woodstock Park",
+            "image_url": "https://s3-media3.fl.yelpcdn.com/bphoto/i1ulHzpnw65oSMzUSIJIyA/o.jpg",
+            "is_closed": false,
+            "url": "https://www.yelp.com/biz/woodstock-park-portland?adjust_creative=Q9m-GQZI6WeFiuWS3dtooQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Q9m-GQZI6WeFiuWS3dtooQ",
+            "review_count": 19,
+            "categories": [
+                {
+                    "alias": "dog_parks",
+                    "title": "Dog Parks"
+                },
+                {
+                    "alias": "playgrounds",
+                    "title": "Playgrounds"
+                }
+            ],
+            "rating": 4,
+            "coordinates": {
+                "latitude": 45.4951335624576,
+                "longitude": -122.6145267
+            },
+            "transactions": [],
+            "location": {
+                "address1": "SE 47th Ave",
+                "address2": "",
+                "address3": "",
+                "city": "Portland",
+                "zip_code": "97206",
+                "country": "US",
+                "state": "OR",
+                "display_address": [
+                    "SE 47th Ave",
+                    "Portland, OR 97206"
+                ]
+            },
+            "phone": "",
+            "display_phone": "",
+            "distance": 1686.720929616
+        },
+        {
+            "id": "berkeley-park-portland",
+            "name": "Berkeley Park",
+            "image_url": "https://s3-media2.fl.yelpcdn.com/bphoto/RbXwscIRvF1LI87qqexwFw/o.jpg",
+            "is_closed": false,
+            "url": "https://www.yelp.com/biz/berkeley-park-portland?adjust_creative=Q9m-GQZI6WeFiuWS3dtooQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Q9m-GQZI6WeFiuWS3dtooQ",
+            "review_count": 3,
+            "categories": [
+                {
+                    "alias": "parks",
+                    "title": "Parks"
+                }
+            ],
+            "rating": 4.5,
+            "coordinates": {
+                "latitude": 45.472587,
+                "longitude": -122.613477
+            },
+            "transactions": [],
+            "location": {
+                "address1": "SE 39th Ave Bybee Blvd",
+                "address2": "",
+                "address3": "",
+                "city": "Portland",
+                "zip_code": "97202",
+                "country": "US",
+                "state": "OR",
+                "display_address": [
+                    "SE 39th Ave Bybee Blvd",
+                    "Portland, OR 97202"
+                ]
+            },
+            "phone": "+15038232223",
+            "display_phone": "(503) 823-2223",
+            "distance": 1725.805199922
+        },
+        {
+            "id": "creston-park-portland",
+            "name": "Creston Park",
+            "image_url": "https://s3-media1.fl.yelpcdn.com/bphoto/GC2E6JvdsBad9toeDJWozQ/o.jpg",
+            "is_closed": false,
+            "url": "https://www.yelp.com/biz/creston-park-portland?adjust_creative=Q9m-GQZI6WeFiuWS3dtooQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=Q9m-GQZI6WeFiuWS3dtooQ",
+            "review_count": 11,
+            "categories": [
+                {
+                    "alias": "swimmingpools",
+                    "title": "Swimming Pools"
+                },
+                {
+                    "alias": "dog_parks",
+                    "title": "Dog Parks"
+                }
+            ],
+            "rating": 3.5,
+            "coordinates": {
+                "latitude": 45.4975149035454,
+                "longitude": -122.617030888796
+            },
+            "transactions": [],
+            "location": {
+                "address1": "4476 SE Powell Blvd",
+                "address2": "",
+                "address3": "",
+                "city": "Portland",
+                "zip_code": "97206",
+                "country": "US",
+                "state": "OR",
+                "display_address": [
+                    "4476 SE Powell Blvd",
+                    "Portland, OR 97206"
+                ]
+            },
+            "phone": "+15038233647",
+            "display_phone": "(503) 823-3647",
+            "distance": 1986.408040132
+        }
+    ],
+    "total": 40,
+    "region": {
+        "center": {
+            "longitude": -122.5991821289,
+            "latitude": 45.4844354741
+        }
+    }
+}
+
+const YELP_PARK_SETTINGS = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://api.yelp.com/v3/businesses/search?location=97206&categories=parks&radius=16000&sort_by=distance",
+	"method": "GET",
+	"headers": {
+		"authorization": "Bearer yzvfmEeIjLbTGkUD0BH96kqjoY53lS-oXCvBC_kc1lO9d1g6jSYuV3tjQXwJmbjK88sCr_VP6irssyn6LqGWqEz6lHjig6ba1NdThIjxssBoT2H5-8EmIRNB6-j8WXYx",
+		"cache-control": "no-cache",
+		// "postman-token": "d14551b1-8ef4-ba27-67ad-e51cc73d2c7a"
+	}
+};
+
+function helloWorld() {
+	// $('body').append(`<p>(hello world)</p>`)
 	console.log ('hello world');
 }
 
-$(picnicInThePark())
+function showSection (section) {
+	$('section').not(section).addClass('hidden');
+	$(section).removeClass('hidden');
+}
+
+function renderYelpResults
+
+
+
+function makeYelpCall(settings, functionOnSuccess) {
+	$.getJSON(settings, functionOnSuccess);
+	// $.ajax(settings).done(functionOnSuccess);
+}
+
+// $.ajax(settings).done(function (response) {
+//   console.log(response);
+// });
+
+
+
+function testApiCall () {
+	var settings = {
+	  "async": true,
+	  "crossDomain": true,
+	  "url": "https://api.yelp.com/v3/businesses/search?location=97206&categories=parks&radius=16000&sort_by=distance&limit=10",
+	  "method": "GET",
+	  "headers": {
+	    "authorization": "Bearer yzvfmEeIjLbTGkUD0BH96kqjoY53lS-oXCvBC_kc1lO9d1g6jSYuV3tjQXwJmbjK88sCr_VP6irssyn6LqGWqEz6lHjig6ba1NdThIjxssBoT2H5-8EmIRNB6-j8WXYx",
+	    // "cache-control": "no-cache",
+	    // "postman-token": "d8431675-1487-2c2f-1721-de0ac438fbd2"
+	  }
+	};
+	$.ajax(settings).done(function (response) {
+	  console.log(response);
+	});
+}
+
+function watchLocationSubmit() {
+	$('.js-location-form').submit(event => {
+		event.preventDefault();
+		let locationTarget = $(event.currentTarget).find('.js-starting-location');
+		let entryLocation = locationTarget.val();
+		locationTarget.val("");
+	// send this off to the API call, render the results, etc
+
+		showSection('.js-pick-park-section');
+	}
+)}
+
+
+function picnicInThePark() {
+	watchLocationSubmit();
+}
+
+$(picnicInThePark)
